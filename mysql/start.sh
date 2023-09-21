bc3af78cca2eec7ab9a4500a3f17f863c4e3ab79
deukjin kim <djkim@ex-em.com>
2023-09-21 15:34:44 +0900

    [fea] script 수정함
    :Why/What:
    script 수정함

--- mysql/start.sh
+++ mysql/start.sh
@@ -2,17 +2,17 @@ export TMP=`pwd`/TMP
 mkdir -p $TMP
 echo "BUILD HAMMERDB SCHEMA"
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
-./hammerdbcli py auto ./mysql_tprocc_buildschema.py
+./hammerdbcli py auto /tpcc_script/mysql_tprocc_buildschema.py
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 
 
 echo "RUN HAMMERDB TEST"
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
-./hammerdbcli py auto ./mysql_tprocc_run.py
+./hammerdbcli py auto /tpcc_script/mysql_tprocc_run.py
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 
 
 echo "DROP HAMMERDB SCHEMA"
-./hammerdbcli py auto ./mysql_tprocc_deleteschema.py
+./hammerdbcli py auto /tpcc_script/mysql_tprocc_deleteschema.py
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 
