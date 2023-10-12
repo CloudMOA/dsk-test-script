b09411069fce52b0afeb14d418bccee3aa6adc5b
deukjin kim <djkim@ex-em.com>
2023-10-12 16:03:28 +0900

    [fea] lock query 경합되도록 process 추가
    :Why/What:
    lock query 경합되도록 process 추가함.

--- start.sh
+++ start.sh
@@ -12,6 +12,10 @@ python /db_script/pg_lock_run_2.py &
 echo "Running pg_lock_run_2.py"
 sleep 1
 
+python /db_script/pg_lock_run_2.py &
+echo "Running pg_lock_run_2.py"
+sleep 1
+
 python /db_script/maria_run.py &
 echo "Running maria_run.py"
 sleep 1
@@ -24,12 +28,20 @@ python /db_script/maria_lock_run_2.py &
 echo "Running maria_lock_run_2.py"
 sleep 1
 
+python /db_script/maria_lock_run_2.py &
+echo "Running maria_lock_run_2.py"
+sleep 1
+
 python /db_script/mysql_run.py &
 echo "Running mysql_run.py"
 sleep 1
 
 python /db_script/mysql_lock_run_1.py &
 echo "Running mysql_lock_run_1.py"
+sleep 1
+
+python /db_script/mysql_lock_run_2.py &
+echo "Running mysql_lock_run_2.py"
 sleep 1
 
 python /db_script/mysql_lock_run_2.py &
