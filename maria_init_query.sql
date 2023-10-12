a82e327e01ee1d4d1662993844a0d2d1f90254e3
deukjin kim <djkim@ex-em.com>
2023-10-12 17:47:43 +0900

    [fea] Init 옵션 수정
    :Why/What:
    Init 옵션 수정함.

--- maria_init_query.sql
+++ maria_init_query.sql
@@ -1,3 +1,5 @@
 
 UPDATE performance_schema.setup_consumers SET ENABLED='YES' WHERE NAME LIKE 'statements_current';
 SET GLOBAL innodb_undo_log_truncate=ON;
+SET GLOBAL max_connections=300;
+SET GLOBAL wait_timeout=180;
--- mysql_init_query.sql
+++ mysql_init_query.sql
@@ -2,3 +2,5 @@
 UPDATE performance_schema.setup_consumers SET ENABLED='YES' WHERE NAME LIKE 'statements_current';
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'root';
 SET GLOBAL innodb_undo_log_truncate=ON;
+SET GLOBAL max_connections=300;
+SET GLOBAL wait_timeout=180;
