25b3c689ae5ddce6c28bf7322bb6c7218bdf2b63
deukjin kim <djkim@ex-em.com>
2023-11-24 16:01:38 +0900

    [fea] Maria load 발생 쿼리 수정
    :Why/What:
    Naria load 발생 쿼리 수정함.

--- maria_load_query.sql
+++ maria_load_query.sql
@@ -5,12 +5,6 @@ group by customer.c_id
 order by customer.c_id
 limit 100;
 
-select * from orders inner join customer c on orders.o_w_id = c.c_w_id and orders.o_d_id = c.c_d_id and orders.o_c_id = c.c_id
-inner join history h on c.c_w_id = h.h_c_w_id and c.c_d_id = h.h_c_d_id and c.c_id = h.h_c_id
-inner join district d on c.c_w_id = d.d_w_id and c.c_d_id = d.d_id
-order by d.d_w_id, c.c_credit_lim
-limit 100;
-
 select * from stock
 inner join item i on stock.s_i_id = i.i_id
 inner join order_line ol on stock.s_w_id = ol.ol_supply_w_id and stock.s_i_id = ol.ol_i_id
